4ebb01d16636be877c2d7f86e928e55c0f07c161
JeongYeop <117172805+ITfervor@users.noreply.github.com>
2024-10-19 09:02:02 +0900

    Update SajuInfoRepository.java

--- src/main/java/com/example/sajucombi/sajuinfo/repository/SajuInfoRepository.java
+++ src/main/java/com/example/sajucombi/sajuinfo/repository/SajuInfoRepository.java
@@ -3,6 +3,6 @@ package com.example.sajucombi.sajuinfo.repository;
 import com.example.sajucombi.sajuinfo.entity.SajuDb;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SajuInfoRepository extends JpaRepository<SajuDb, Long> {
+public interface SajuDBRepository extends JpaRepository<SajuDb, Long> {
 
 }
